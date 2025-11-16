3e3b7bb8897714ccfa704677f89121ee7575b375
Olha Fursova <teor15manypeople1@gmail.com>
2025-11-16 20:23:45 +0100

    Tasks Done

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,16 @@
 let some:unknown;
 some = 'Text';
+
+// 1. Приведення типу
+
 let str: string;
-str = some;
+str = some as string;
+
+// 2. Додавання перевірки
+
+// let str: string;
+// if (typeof some === "string") {
+//   str = some;
+// }
 
 export {};
